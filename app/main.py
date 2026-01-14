1668deb6eda6d6330bcaba8913b1c0284dca1d8e
titoiriepal <titoiriepal@hotmail.com>
2026-01-14 10:20:08 +0100

    Magic methods lg, gt, eq, le and ge added

--- app/main.py
+++ app/main.py
@@ -8,20 +8,35 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: "Distance") -> "Distance":
+    def __add__(self, other: Distance | int | float) -> Distance:
         return Distance(self.km + other.km)
 
-    def __iadd__(self, other: "Distance") -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         self.km += other.km
         return self
 
-    def __mul__(self, factor: int) -> "Distance":
+    def __mul__(self, factor: int) -> Distance:
         return Distance(self.km * factor)
 
-    def __truediv__(self, divisor: int) -> "Distance":
+    def __truediv__(self, divisor: int) -> Distance:
         return Distance(round(self.km / divisor, 2))
+
+    def __lt__(self, other: Distance | int | float) -> bool:
+        return self.km < other.km
+
+    def __gt__(self, other: Distance | int | float) -> bool:
+        return self.km > other.km
+
+    def __eq__(self, other: Distance | int | float) -> bool:
+        return self.km == other.km
+
+    def __le__(self, other: Distance | int | float) -> bool:
+        return self.km <= other.km
+
+    def __ge__(self, other: Distance | int | float) -> bool:
+        return self.km >= other.km
 
 
 distance = Distance(30)
-distance2 = distance / 6
-print(distance2)
+distance2 = Distance(23)
+print(distance > distance2)  # True
