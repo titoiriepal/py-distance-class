bf6ed216077ae608d82ccf65b70e6298882d0c91
titoiriepal <titoiriepal@hotmail.com>
2026-01-14 10:25:45 +0100

    Types int and float erased from definitions of methods

--- app/main.py
+++ app/main.py
@@ -8,10 +8,10 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int | float) -> Distance:
+    def __add__(self, other: Distance) -> Distance:
         return Distance(self.km + other.km)
 
-    def __iadd__(self, other: Distance | int | float) -> Distance:
+    def __iadd__(self, other: Distance) -> Distance:
         self.km += other.km
         return self
 
@@ -21,22 +21,17 @@ class Distance:
     def __truediv__(self, divisor: int) -> Distance:
         return Distance(round(self.km / divisor, 2))
 
-    def __lt__(self, other: Distance | int | float) -> bool:
+    def __lt__(self, other: Distance) -> bool:
         return self.km < other.km
 
-    def __gt__(self, other: Distance | int | float) -> bool:
+    def __gt__(self, other: Distance) -> bool:
         return self.km > other.km
 
-    def __eq__(self, other: Distance | int | float) -> bool:
+    def __eq__(self, other: Distance) -> bool:
         return self.km == other.km
 
-    def __le__(self, other: Distance | int | float) -> bool:
+    def __le__(self, other: Distance) -> bool:
         return self.km <= other.km
 
-    def __ge__(self, other: Distance | int | float) -> bool:
+    def __ge__(self, other: Distance) -> bool:
         return self.km >= other.km
-
-
-distance = Distance(30)
-distance2 = Distance(23)
-print(distance > distance2)  # True
