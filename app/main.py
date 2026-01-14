156834b3489476a9fc7e5f7f0c07ac68a293099b
titoiriepal <titoiriepal@hotmail.com>
2026-01-14 09:57:54 +0100

    Magic methods init, str, repr and add added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,18 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km: int) -> None:
+        self.km = km
+
+    def __str__(self) -> str:
+        return f"Distance: {self.km} kilometers"
+
+    def __repr__(self) -> str:
+        return f"Distance(km={self.km})"
+
+    def __add__(self, other: "Distance") -> "Distance":
+        return Distance(self.km + other.km)
+
+
+distance = Distance(5)
+distance2 = Distance(10)
+distance3 = distance + distance2
+print(distance3)
